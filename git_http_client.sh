002f6181fde49bd9e9adc330d261e19311cc1472
alx22312 <aaaa9z@yandex.ru>
2015-01-29 06:25:47 +0500

    2 req/res work second fail

--- git_http_client.sh
+++ git_http_client.sh
@@ -1,3 +1,3 @@
-cd f:; cd C; cd http; cd http_client; 
-git add .;git commit -a;git push server3 master:master;
+cd f:; cd C; cd http; cd http_client;
+git add .;git commit -a;git push server2 master:master;
 read -p "-End-"
